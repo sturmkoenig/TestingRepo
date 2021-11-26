66ecbbbec140165857b53fcf4427a1d8e463a25d
llauer <llauer@host.docker.internal>
2021-11-26 10:47:21 +0100

    Fixed Wrong import to reflect the usage of EJB

--- src/main/java/com/airhacks/services/LoggerProducer.java
+++ src/main/java/com/airhacks/services/LoggerProducer.java
@@ -1,9 +1,9 @@
 package com.airhacks.services;
 
+import javax.enterprise.inject.Produces;
 import javax.enterprise.inject.spi.InjectionPoint;
 import javax.inject.Named;
 import javax.inject.Singleton;
-import javax.ws.rs.Produces;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
